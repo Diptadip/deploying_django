840504e4085dabefa484707c4f4f4f06afceae66
Diptadip Mallik <diptadip2105@gmail.com>
2024-11-18 01:53:55 +0530

    Update views.py

--- sub_app/views.py
+++ sub_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the sub_app index. checking webhook.")
+    return HttpResponse("Hello, world.  You're at the sub_app index. checking webhook itegration.")
 
